d87571cc2129fbe5d809cfa4a1c3540aaf6ccdeb
Manuel Beeler <mbeeler@c2r2p1.hive.fi>
2021-10-29 14:43:51 +0300

    ex13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbeeler <mbeeler@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 12:10:13 by mbeeler           #+#    #+#             */
+/*   Updated: 2021/10/29 14:41:52 by mbeeler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	else
+	{
+		nb *= ft_recursive_factorial(nb - 1);
+		return (nb);
+	}
+}

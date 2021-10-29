cea4163c8f3ea8950b15e3ef8a6a0d2313baa9d2
Manuel Beeler <mbeeler@c2r2p1.hive.fi>
2021-10-29 12:32:17 +0300

    ex06

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbeeler <mbeeler@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 12:10:13 by mbeeler           #+#    #+#             */
+/*   Updated: 2021/10/29 12:26:38 by mbeeler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}

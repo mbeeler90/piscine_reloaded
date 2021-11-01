3879bf78647b02529304858abdd265b60f036566
Manuel Beeler <manuel.beeler90@gmail.com>
2021-11-01 21:38:44 +0200

    ex23

---
+++ ex23/ft_point.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manuelbeeler <manuelbeeler@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 12:10:13 by mbeeler           #+#    #+#             */
+/*   Updated: 2021/11/01 20:06:35 by manuelbeele      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+struct s_point
+{
+	int	x;
+	int	y;
+};
+
+typedef struct s_point	t_point;
+
+#endif

28cc40dbaf95afd446d0655873abb0371bbdc598
Manuel Beeler <manuel.beeler90@gmail.com>
2021-11-02 10:08:04 +0200

    ex26

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -1,20 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: manuelbeeler <manuelbeeler@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 12:10:13 by mbeeler           #+#    #+#             */
-/*   Updated: 2021/11/02 10:07:10 by manuelbeele      ###   ########.fr       */
+/*   Updated: 2021/11/02 10:06:29 by manuelbeele      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_foreach(int *tab, int length, void (*f)(int))
+int	ft_count_if(char **tab, int (*f)(char*))
 {
 	int	i;
+	int	count;
 
+	count = 0;
 	i = -1;
-	while (++i < length)
-		f(tab[i]);
+	while (tab[++i])
+	{
+		if (f(tab[i]) == 1)
+			count++;
+	}
+	return (count);
 }
